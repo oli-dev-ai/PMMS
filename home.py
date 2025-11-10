537976eb067120e44fc25afabb522d3997cef969
oli-dev-ai <oopyrchal@pm.me>
2025-11-10 21:46:20 +0100

    pierwsze ulepszenia strony

---
+++ home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+
+st.title("Route Calculator")
+st.write(
+    "App for calculating cost of transport")
+
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
-st.title("Route Calculator")
-st.write(
-    "App for calculating cost of transport"
-)
+#definiowanie stron
+home = st.Page("home.py", title="Home", icon=":material/home:")
+dashboard = st.Page("dashboard.py", title="Dashboard", icon=":material/dashboard:")
+route_calculator = st.Page("routcalc.py", title="Route Calculator", icon=":material/calculate:")
+
+pg = st.navigation([home, dashboard, route_calculator])
+pg.run()
+
+
